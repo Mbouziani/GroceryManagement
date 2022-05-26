50415fd8844899826428903d59531c6fd7969723
Bouziani Mohammed <bouzimoha150@gmail.com>
2022-05-26 21:27:54 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,45 @@
+using GroceryManagement.Models;
+using Microsoft.EntityFrameworkCore;
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+
+builder.Services.AddDbContext<MarketManagementDBContext>(
+    option => option.UseSqlServer(builder.Configuration.GetConnectionString("dbconection")));
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddCors(options => options.AddPolicy("ApiCorsPolicy", builder =>
+{
+    builder.WithOrigins("https://aljazeramax.com").AllowAnyMethod().AllowAnyHeader();
+}));
+
+
+builder.Services.AddMvc(options => options.EnableEndpointRouting = false);
+builder.Services.AddMvc();
+
+var app = builder.Build();
+
+app.UseCors(builder => builder
+     .AllowAnyOrigin()
+     .AllowAnyMethod()
+     .AllowAnyHeader());
+app.UseMvc();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
